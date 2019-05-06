ea8719c776f7b3259204466680f9a64465f5889c
LFox42 <47540294+LFox42@users.noreply.github.com>
2019-05-07 10:33:43 +1200

    Add files via upload

---
+++ School-Canteen/School_Canteen.py
@@ -0,0 +1,57 @@
+
+
+#Build log for Sign up web page
+#Ver 1.1 - Created TEST data for Ticket variable
+#Ver 1.2 - Adding server functionality to python
+
+
+from bottle import run, route, view, get, post, request, static_file
+from itertools import count
+
+###Class START WITH CAPITAL LETTERS
+
+class Food:
+    
+    # _ signifies a private variable. not to be used outside of this class.
+    _ids = count (0)
+    
+    def __init__(self, name, image, amount): 
+        #not passing ID as we want it to create it.
+        self.id = next(self._ids)
+        self.name = name
+        self.image = image
+        self.amount = amount
+
+
+#Data on products in stock.
+store_test = [
+    Food("Sushi Roll Pack", "image", 5),
+    Food("Hot Dog and Chips", "image", 12),
+    Food("Ham and Cheese Sandwiches", "image",4),
+    ]
+
+#Pages
+
+#Store page
+@route('/')
+@view ('Purchase_Page')
+def Purchase():
+    #buy and restock books
+    data = dict (store_list = store_test)
+    return data
+
+@route('/purchase-success/<food_id>')
+@view('purchase-success')
+def purchase_success(food_id):
+    food_id = int(food_id)
+    found_food = None
+    for food in store_test:
+        if food.id == food_id:
+            found_food = food
+    data = dict (food = found_food)
+    found_food.amount = found_food.amount - 1
+    return data
+
+
+
+run(host='0.0.0.0', port = 8080, reloader=True, debug=True)
