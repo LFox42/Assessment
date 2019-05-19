ca6e28d8707c889acdf22ab6ab84079e6ef1956a
LFox42 <47540294+LFox42@users.noreply.github.com>
2019-05-20 11:49:05 +1200

    Add files via upload

--- School-Canteen/School_Canteen.py
+++ School-Canteen/School_Canteen.py
@@ -40,6 +40,7 @@ def Purchase():
     data = dict (store_list = store_test)
     return data
 
+#the purshase was successful.
 @route('/purchase-success/<food_name>')
 @view('purchase-success')
 def purchase_success(food_name):
@@ -51,6 +52,7 @@ def purchase_success(food_name):
     found_food.amount = found_food.amount - 1
     return data
 
+#to input the amount to restock
 @route('/restock/<food_name>')
 @view('restock')
 def Restock(food_name):
@@ -61,6 +63,7 @@ def Restock(food_name):
             found_food = food    
     curr_food = found_food
 
+#to restock the inputted amount
 @route('/restock_success', method="POST")
 @view('restock_success')
 def restock_success():
