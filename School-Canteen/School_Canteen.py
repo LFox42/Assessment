c1e494bc40de215a2847e49cd8398b3322c5cea8
LFox42 <47540294+LFox42@users.noreply.github.com>
2019-06-21 12:32:18 +1200

    Add files via upload

--- School-Canteen/School_Canteen.py
+++ School-Canteen/School_Canteen.py
@@ -1,8 +1,9 @@
 
 
-#Build log for Sign up web page
-#Ver 1.1 - Created TEST data for Ticket variable
-#Ver 1.2 - Adding server functionality to python
+#School Canteen
+
+#Ver 1.0: Create bottle server, create classes
+
 
 
 from bottle import run, route, view, get, post, request, static_file
@@ -40,5 +41,5 @@ def index():
     pass
 
 
-
+#address to main page (localhost:8080)
 run(host='0.0.0.0', port = 8080, reloader=True, debug=True)
