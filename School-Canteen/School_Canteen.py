772d1b03ddeb2b2bbd0e6c9b1c8edeb050993b39
LFox42 <47540294+LFox42@users.noreply.github.com>
2019-06-21 12:21:32 +1200

    Add files via upload

--- School-Canteen/School_Canteen.py
+++ School-Canteen/School_Canteen.py
@@ -1,8 +1,11 @@
 
+#School Canteen
 
-#Build log for Sign up web page
-#Ver 1.1 - Created TEST data for Ticket variable
-#Ver 1.2 - Adding server functionality to python
+#Ver 1.0: Create bottle server, create classes
+#Ver 2.0: Create HTML for 
+#Ver 3.0: Add Purchase/Restock pages
+#Ver 4.0: Add restock imput
+
 
 
 from bottle import run, route, view, get, post, request, static_file
@@ -36,7 +39,7 @@ curr_food = None #store_test[0]
 @route('/')
 @view ('Purchase_Page')
 def Purchase():
-    #buy and restock books
+    #buy and restock Product
     data = dict (store_list = store_test)
     return data
 
@@ -44,6 +47,7 @@ def Purchase():
 @route('/purchase-success/<food_name>')
 @view('purchase-success')
 def purchase_success(food_name):
+    #removes 1 food    
     found_food = None
     for food in store_test:
         if food.name == food_name:
@@ -73,5 +77,5 @@ def restock_success():
 
 
 
-
+#address to main page (localhost:8080)
 run(host='0.0.0.0', port = 8080, reloader=True, debug=True)
